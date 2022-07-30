b6bf696aafa72fe6b38fc39644f859db2bc95aee
DemhukVit <demchukvitalii.kr@gmail.com>
2022-07-31 00:01:49 +0200

    HW4 done

--- JS/HW4.js
+++ JS/HW4.js
@@ -24,8 +24,8 @@
 // const arr1 = new Array(...arr);
 // let multiplication = multiplicationNumber(arr1);
 // console.log(multiplication);
-// function multiplicationNumber(array) {
-//     return array.map(function(num) {
+// function multiplicationNumber(arr1) {
+//     return arr1.map(function(num) {
 //         return num * 10;
 //     });
 // }
@@ -46,7 +46,7 @@
 //     console.log(i)
 // }
 //==============================================Задание №10==================================//
-//  for (var i = -4; i <= 100; i += 1) {
+//  for (var i = -4; i <= 100; i += 2) {
 //      console.log(i)
 //  }
 //==============================================Задание №11==================================//
@@ -70,7 +70,7 @@
 // let n = +prompt('Введите длину масива')
 // const arr = [];
 // let str;
-// for (var i=1; i<n; i++) {
+// for (var i=1; i<=n; i++) {
 //     str = '';
 //     for (var j=1; j<=i; j++) {
 //         str += i;
@@ -79,3 +79,77 @@
 // }
 // console.log(arr);
 //==============================================Задание №14==================================//
+// let arr = [];
+// for (let i = 1; i < 11; i++) {
+//     const testArray = [];
+//     for (let j = 1; j < 11; j++) {
+//         let el = i === j - j + 10 ? 'x' : i === i - j +10 ? 'x' : i === j - j +1 ? 'x' : i === i - j +1 ? 'x' : '0';
+//
+//         testArray.push(el);
+//
+//     }
+//     arr.push(testArray);
+// }
+// console.log(arr, 'arr')
+//==============================================Задание №15==================================//
+// var output = '';
+// for (var i = 1; i <= 10; i++) {
+//     for (var j = 1; j <= 10; j++) {
+//         output += ' ' + i * j;
+//         if (i * j < 11) {
+//             output += ' ';
+//         }
+//     }
+//     console.log(output);
+//    output = '';
+// }
+//============================================================================================//
+// let arr = [];
+// // let arrHeight = 9;
+// // let arrWidth = 9;
+//
+// for (let i = 0; i <= 9; i++) {
+//     arr[i] = [];
+//     for (let j = 0; j <= 9; j++) {
+//         arr[i][j] = (j + 1) * (1 + i);
+//
+//     }
+// }
+// console.log(arr);
+//==============================================Задание №16==================================//
+// let Nubmer = +prompt('Введите количество Ваших товаров:');
+// const arrWords = [' товар', ' товара', ' товаров']
+// function word(Number) {
+//     let result
+//     let count = Nubmer % 100;
+//     if (count >= 5 && count <= 20) {
+//         result = arrWords['2'];
+//     } else {
+//         count = count % 10;
+//
+//         if (count === 1) {
+//             result = arrWords['0'];
+//         } else if (count >= 2 && count <= 4) {
+//             result = arrWords['1'];
+//         } else {
+//             result = arrWords['2']
+//         }
+//     }
+//     return result;
+// }
+// alert(`В вашей корзине ${Nubmer}` + word());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
